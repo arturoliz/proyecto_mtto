c4873e9e67b2b8bcced336bd07b395ea1f44ad61
Francisco Mejia <franjmr1@gmail.com>
2025-05-22 19:41:19 -0700

    metodos funcionales de retirar y transferir en la clase de Cajero

--- CajeroAutomatico.java
+++ CajeroAutomatico.java
@@ -25,4 +25,33 @@ public class CajeroAutomatico {
     public void setBanco(Banco banco) {
         this.banco = banco;
     }
+    
+    public Retiro retirar(Cuenta cuenta, double monto) {
+      if (cuenta.getSaldo() >= monto) {
+          cuenta.setSaldo(cuenta.getSaldo() - monto);
+          Retiro retiro = new Retiro(generarId(), new Date(), monto, cuenta.getCliente(), banco);
+          retiro.imprimirTicket();
+          return retiro;
+      } else {
+          System.out.println("Saldo insuficiente.");
+          return null;
+      }
+  }
+
+  public Transferencia transferir(Cuenta origen, Cuenta destino, double monto) {
+      if (origen.getSaldo() >= monto) {
+          origen.setSaldo(origen.getSaldo() - monto);
+          destino.setSaldo(destino.getSaldo() + monto);
+          Transferencia transferencia = new Transferencia(generarId(), new Date(), monto, origen.getCliente(), banco, destino);
+          transferencia.imprimirTicket();
+          return transferencia;
+      } else {
+          System.out.println("Saldo insuficiente para transferir.");
+          return null;
+      }
+  }
+
+  private String generarId() {
+      return UUID.randomUUID().toString();
+  }
 }
